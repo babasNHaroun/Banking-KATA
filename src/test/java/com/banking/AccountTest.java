0c6a158da42fd0f0eebb505e5144497a32203b2d
saidanir <tamouhamb@gmail.com>
2025-05-31 15:05:02 +0200

    US-2: withdraw money + unit tests + minimal code to pass the tests

--- src/test/java/com/banking/AccountTest.java
+++ src/test/java/com/banking/AccountTest.java
@@ -19,12 +19,30 @@ public class AccountTest {
 
     @Test
     void shouldIncreaseBalanceWhenDepositing() {
-        account.deposit(100);
-        assertEquals(100, account.getBalance());
+        account.deposit(1000);
+        assertEquals(1000, account.getBalance());
     }
 
     @Test
     void shouldThrowExceptionWhenDepositingNegativeAmount() {
-        assertThrows(IllegalArgumentException.class, () -> account.deposit(-100));
+        assertThrows(IllegalArgumentException.class, () -> account.deposit(-1000));
+    }
+
+     @Test
+    void shouldDecreaseBalanceWhenWithdrawing() {
+        account.deposit(1000);
+        account.withdraw(300);
+        assertEquals(700, account.getBalance());
+    }
+
+    @Test
+    void shouldThrowExceptionWhenWithdrawingNegativeAmount() {
+        assertThrows(IllegalArgumentException.class, () -> account.withdraw(-1000));
+    }
+
+    @Test
+    void shouldThrowExceptionWhenWithdrawingMoreThanBalance() {
+        account.deposit(500);
+        assertThrows(IllegalArgumentException.class, () -> account.withdraw(1000));
     }
 }
