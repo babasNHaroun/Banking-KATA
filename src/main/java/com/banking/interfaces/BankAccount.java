588791e94eb40626521ba3d8ac5081a1f8531c72
saidanir <tamouhamb@gmail.com>
2025-06-01 20:57:02 +0200

    adding a bit of documentation

--- src/main/java/com/banking/interfaces/BankAccount.java
+++ src/main/java/com/banking/interfaces/BankAccount.java
@@ -3,9 +3,24 @@ package com.banking.interfaces;
 import java.math.BigDecimal;
 
 public interface BankAccount {
+    /**
+     * Returns the current balance of the account.
+     * 
+     * @return The current balance.
+     */
     BigDecimal getBalance();
 
+    /**
+     * Deposits an amount into the account.
+     * 
+     * @param amount The amount to deposit.
+     */
     void deposit(BigDecimal amount);
 
+    /**
+     * Withdraws an amount from the account.
+     * 
+     * @param amount The amount to withdraw.
+     */
     void withdraw(BigDecimal amount);
 }
--- src/main/java/com/banking/interfaces/StatementPrinter.java
+++ src/main/java/com/banking/interfaces/StatementPrinter.java
@@ -4,5 +4,11 @@ import java.util.List;
 import com.banking.model.Transaction;
 
 public interface StatementPrinter {
+    /**
+     * Prints a statement of transactions.
+     * 
+     * @param transactions The list of transactions to print.
+     * @return The statement as a string.
+     */
     String print(List<Transaction> transactions);
 }
